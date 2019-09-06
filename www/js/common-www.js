43d916827dd7f40bf3d8c6557ff12fe9d312357e
hongsun <hongsun3152@gmail.com>
2019-09-06 17:15:34 +0900

    현황판 업데이트

--- www/js/common-www.js
+++ www/js/common-www.js
@@ -95,12 +95,25 @@ var mobileGnb = function () {
   });
 };
 
+// DatePicker
+var setDatePicker = function() {
+  $.datepicker.setDefaults({
+    dateFormat: 'yy-mm-dd',
+    dayNames: ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일'],
+    dayNamesMin: ['월', '화', '수', '목', '금', '토', '일'],
+    monthNames: ['1월', '2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
+    monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+  });
+  $('.inp_date').datepicker();
+};
 
 // TypeB Main page JS
 $(document).ready(function () {
   setGnb();
   mobileGnb();
   new WOW().init();
+  
+  if ($('.inp_date').length) setDatePicker();
 
   $('body').on('mousewheel DOMMouseScroll', function(e){
     if(typeof e.originalEvent.detail == 'number' && e.originalEvent.detail !== 0) {
