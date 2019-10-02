9592ee364f6218d5d69bf7df4392ed012da70c04
cd-mk <cd-mk@cidow.com>
2019-10-03 04:21:58 +0900

    슬라이드 수정

--- www/assets/js/common-mall.js
+++ www/assets/js/common-mall.js
@@ -64,16 +64,6 @@ var galleryThumbs = new Swiper('.pdt_thumbs', {
 	watchSlidesVisibility: true,
 	watchSlidesProgress: true,
 });
-var galleryTop = new Swiper('.photo_box', {
-	spaceBetween: 10,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	thumbs: {
-		swiper: galleryThumbs
-	}
-});
 
 
 $(document).ready(function () {
