1fdc246845e62d39543324e6204386047a2a6fa1
cd-mk <cd-mk@cidow.com>
2019-10-19 00:36:38 +0900

    디지털복사 슬라이드 및 공통 여백 수정

--- www/assets/js/sub.js
+++ www/assets/js/sub.js
@@ -3,51 +3,25 @@ function slideInit() {
   var isMobile = chkViewport();
 
   // 디지털출력 복사  
-  (function () {
-    var pcOpt = {
-      slidesPerView: 'auto',
-      roundLengths: true,
-      spaceBetween: 30,
-      loop: false,
-      loopFillGroupWithBlank: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        hide: false,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-      }
-    };
-    var moOpt = {
-      slidesPerView: 'auto',
-      spaceBetween: 15,
-      roundLengths: true,
-      loop: false,
-      loopFillGroupWithBlank: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        hide: false,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-      }
-    };
+  var servicePdt = new Swiper('.service_info_view', {
+    spaceBetween: 30,
+    slidesPerView: 'auto',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      hide: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction',
+    }
+  });
 
-    var service_info_view = isMobile
-      ? new Swiper('.service_info_view', moOpt)
-      : new Swiper('.service_info_view', pcOpt);
-    reloadSlide(service_info_view, pcOpt, moOpt);
-  })();
+
+
 
 
   var aa = new Swiper('.product_list', {
@@ -67,7 +41,6 @@ function slideInit() {
   });
 
   //상품 상세보기 슬라이드
-  var isMobile = chkViewport();
 
   var pdtOther = new Swiper('.pdt_slide', {
     slidesPerView: 'auto',
@@ -97,11 +70,3 @@ function slideInit() {
 
 } 
 $(document).ready(slideInit());
-
-
-
-
-
-
-
-
